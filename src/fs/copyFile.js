c4c17f3b25f12f77d2306d4143c1d9ca89cb71c8
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-06-23 22:28:43 +0300

    feat: add copy file

---
+++ src/fs/copyFile.js
@@ -0,0 +1,40 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { showCurrentPath } from "../utils/showCurrentPath.js";
+import { isAbsolute, join, basename, extname } from "node:path";
+
+export const copyFile = async (currentPath, pathData) => {
+  const trimPathData = pathData.trim();
+  const [pathToFile, pathToChooseDir] = trimPathData.split(" ");
+  let fullPath = "";
+  let fullDirPath = "";
+
+  try {
+    if (isAbsolute(pathToFile)) {
+      fullPath = join(pathToFile);
+    } else {
+      fullPath = join(currentPath, pathToFile);
+    }
+
+    const fileExtension = extname(fullPath);
+    const fileName = basename(fullPath, fileExtension);
+    console.log(fileName);
+    if (isAbsolute(pathToChooseDir)) {
+      fullDirPath = join(pathToChooseDir, `${fileName}_copy{${fileExtension}`);
+    } else {
+      fullDirPath = join(
+        currentPath,
+        pathToChooseDir,
+        `${fileName}_copy${fileExtension}`
+      );
+    }
+    console.log(fileExtension);
+    const readStream = createReadStream(fullPath);
+    const writeStream = createWriteStream(fullDirPath);
+    readStream.pipe(writeStream);
+    console.log("\x1b[36m%s\x1b[0m", `File has been copied!`);
+    showCurrentPath(currentPath);
+  } catch (error) {
+    console.error("\x1b[31m%s\x1b[0m", error);
+    showCurrentPath(currentPath);
+  }
+};
