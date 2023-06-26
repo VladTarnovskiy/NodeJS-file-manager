a22b933ec741a3003b5aaf8f8f9ba017eb47fa2d
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-06-26 10:30:22 +0300

    feat: add move func

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import { showCurrentPath } from "./utils/showCurrentPath.js";
 import { upToDirectory } from "./utils/upToDirectory.js";
 import { goToDirectory } from "./utils/goToDirectory.js";
 import { fileList } from "./utils/filesList.js";
+import { getDataCommands } from "./utils/getDataCommand.js";
+import { readFile } from "./fs/readFile.js";
+import { createFile } from "./fs/createFile.js";
+import { renameFile } from "./fs/renameFile.js";
+import { copyFile } from "./fs/copyFile.js";
+import { moveFile } from "./fs/moveFile.js";
+
 let arg = argv[2];
 let username = arg.split("=")[1];
 
@@ -19,10 +26,23 @@ stdin.on("data", async (chunk) => {
     } else if (data.trim() === "up") {
       upToDirectory(currentPath);
     } else if (data.startsWith("cd")) {
-      await goToDirectory(currentPath, data.trim().slice(3));
+      await goToDirectory(currentPath, getDataCommands(data));
     } else if (data.trim() === "ls") {
       await fileList(currentPath);
+    } else if (data.startsWith("cat")) {
+      await readFile(currentPath, getDataCommands(data));
+    } else if (data.startsWith("add")) {
+      await createFile(getDataCommands(data));
+    } else if (data.startsWith("rn")) {
+      await renameFile(currentPath, data.slice(3));
+    } else if (data.startsWith("cp")) {
+      await copyFile(currentPath, data.slice(3));
+    } else if (data.startsWith("mv")) {
+      await moveFile(currentPath, data.slice(3));
     }
+    // else if (data.startsWith("rm")) {
+    //   await fileList(currentPath);
+    // }
   } catch (error) {
     console.log(error);
   }
